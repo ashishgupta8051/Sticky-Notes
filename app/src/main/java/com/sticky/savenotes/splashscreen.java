9db5cd070c4ae7c79bea1cc8c794ce9fd2a107fd
Ashish Gupta <ashishgupta8051@gmail.com>
2021-05-06 16:19:24 +0530

    Change package name

---
+++ app/src/main/java/com/sticky/savenotes/splashscreen.java
@@ -0,0 +1,26 @@
+package com.sticky.savenotes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+
+public class splashscreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splashscreen);
+
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        new Handler().postDelayed(new Runnable() {
+
+            @Override public void run() {
+
+                Intent i = new Intent(splashscreen.this, MainActivity.class); startActivity(i);
+                finish(); } }, 2000);
+    }
+}
